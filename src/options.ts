334911f1e612bf5b823ba001c9365423a1262a94
WittBulter <lambdas.dev@gmail.com>
2020-01-07 20:58:57 +0800

    feat: beautify the string displayed when command starts

--- src/options.ts
+++ src/options.ts
@@ -2,8 +2,10 @@ import chalk from 'chalk'
 const pkg = require('../package.json')
 
 export const help = () => {
-  console.log(`${chalk.cyan('add')} \<command\> -- add a document`)
-  console.log(`${chalk.cyan('-v, --version')} \<option\> -- show version`)
+  console.log(chalk.gray(`> Commands:`))
+  console.log(`  ${chalk.cyan('add')} \<command\> -- add a document`)
+  console.log(`  ${chalk.cyan('ls')}  \<command\> -- show all tags`)
+  console.log(`  ${chalk.cyan('-v, --version')} \<option\> -- show version`)
   console.log('')
 }
 
@@ -14,10 +16,11 @@ export const version = () => {
 export const start = () => {
   const gray = chalk.hex('#a5a5a5')
   const docs = chalk.bold.hex('#e0e0e0')('📦 DOCS')
-  const command = chalk.cyan('docs <keyword>')
-  const add = chalk.cyan('add')
-  console.log(chalk.gray(`  ${docs}`))
-  console.log(gray(`> Run "${command}" to start`))
-  console.log(gray(`> Submit docs with ${add}`))
+  console.log('')
+  console.log(chalk.gray(`    ${docs}`))
+  console.log(chalk.hex('#f0f0f0')('    One command gets all the documents.'))
+  console.log('')
+  console.log(gray(`    - Run "${chalk.cyan('docs <keyword>')}" to start`))
+  console.log(gray(`    - Submit docs with "${chalk.cyan('add')}"`))
   console.log('')
 }
