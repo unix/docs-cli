5073ee703fe6886725a13b4d8978e82ed00a19a4
WittBulter <lambdas.dev@gmail.com>
2020-01-08 21:41:54 +0800

    feat(configs): specify version manually

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -1,3 +1,8 @@
-export const host = 'https://cdn.jsdelivr.net/npm/docs-catalog@latest/'
+/**
+ * The reason for manually specifying the version is better cache consistency
+ * Follow version of docs-catalog
+ * NPM repo:: https://www.npmjs.com/package/docs-catalog
+ */
+export const host = 'https://cdn.jsdelivr.net/npm/docs-catalog@0.0.2/'
 
 export const redirectHost = 'https://docs.codes/'
